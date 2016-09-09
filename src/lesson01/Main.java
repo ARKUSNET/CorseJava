f9ddf06d4a3829ecc6824cbb341d42ee9594127a
Arthur Kuspakov <artur-6272@rambler.ru>
2016-09-09 15:09:11 +0300

    First Commit

---
+++ src/lesson01/Main.java
@@ -0,0 +1,14 @@
+package lesson01;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        Car raceCar = new RaceCar();
+        Car simpleCar = new SimpleCar();
+        simpleCar.setSpeed(200);
+        raceCar.getDescription();
+        System.out.println(simpleCar.getSpeed());
+        simpleCar.getDescription();
+    }
+}
---
+++ src/ru/webapp/model/Link.java
@@ -0,0 +1,14 @@
+package ru.webapp.model;
+
+/**
+ * Created by a.kuspakov on 09.09.2016.
+ */
+public class Link {
+    private final String name;
+    private final String url;
+
+    public Link(String name, String url) {
+        this.name = name;
+        this.url = url;
+    }
+}
